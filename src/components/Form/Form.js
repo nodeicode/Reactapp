79b17c13dfdf1bdaeb031a72fc47485326e3d2cc
nodeicode <lgopik2@uic.edu>
2019-06-14 23:48:22 +0530

    cleaned up !

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,36 +1,33 @@
 import {Component} from 'react';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/styles';
 import './Form.css'
 import '../../../static/index.css'
 
-const a = makeStyles({
-    textField:{
-    width:'30rem',//css for TextField using Styled Compoenets API form material-ui
-    }
-});
 export default function F(){
-    const c = a();
+   // const c = a();
             return(
             <div>
             <header>
                 <h1>Form to Sign Up to the Chat App </h1>
             </header>
-            <form autoComplete="on">
+            <form autoComplete="on" action="Submit">
                 <TextField
                 id="outlined-Email"
                 label="Email"
-                className={c.textField}
+                //className={c.textField}
                 placeholder="Enter your email 🤗"
                 margin="normal"
                 variant="outlined"/>
                 <TextField
                 id="outlined-password"
                 label="Password"
-                className={c.textField}
+                //className={c.textField}
                 placeholder="Now your Password 😁"
                 margin="normal"
                 variant="outlined"/>
+                <Button variant="outlined" >Submit</Button>
             </form>
             </div>
         );
