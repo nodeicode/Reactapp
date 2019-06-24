845d9ebd6090fe6b4b8d8e22ca66660ddb663212
nodeicode <lgopik2@uic.edu>
2019-06-24 23:40:25 +0530

    sensitive info removed

--- src/helpers/passport.js
+++ src/helpers/passport.js
@@ -6,8 +6,8 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 //   credentials (in this case, an accessToken, refreshToken, and Google
 //   profile), and invoke a callback with a user object.
 passport.use(new GoogleStrategy({
-    clientID: '840733934211-5875l2bi0ppttqviuvmu1pt23834586d.apps.googleusercontent.com',
-    clientSecret: 'Ewo9Z9cJeXN2FrkrAwqP91i3',
+    clientID: '',
+    clientSecret: '',
     callbackURL: "http://localhost:3000/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
